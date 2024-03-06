27ad5a9b84fe88a31a79af5825b1653c8cbd5619
wykdg <wykchina@gmail.com>
2024-03-06 20:19:58 +0800

    revert

--- 01_prepare.sh
+++ 01_prepare.sh
@@ -4,7 +4,7 @@
 # 单独拉取mosdns的库
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
