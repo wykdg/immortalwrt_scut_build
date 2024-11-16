07d7a5fe6c42fc9ed3e0232644ded1123c7db84e
wykdg <wykchina@gmail.com>
2024-11-16 17:30:27 +0800

    Update 01_prepare.sh

--- 01_prepare.sh
+++ 01_prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
