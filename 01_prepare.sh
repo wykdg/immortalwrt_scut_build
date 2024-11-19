bfa4045b60c946ddb7b172b5be6f935d7aac6d61
wykdg <wykchina@gmail.com>
2024-11-19 16:24:45 +0800

    Update 01_prepare.sh

--- 01_prepare.sh
+++ 01_prepare.sh
@@ -5,7 +5,7 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
-
+sed -i '20i PKG_USE_MIPS16:=0' feeds/small/dns2tcp/Makefile
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
